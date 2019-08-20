1328d26eeedb46fac89855bbc781802cffeffe6d
Logon Aluno <logonaluno@local.com>
2019-08-19 22:26:12 -0300

    injeçãoDependencias

--- app/src/main/java/br/com/fiap/roomwordsample/MainActivity.kt
+++ app/src/main/java/br/com/fiap/roomwordsample/MainActivity.kt
@@ -3,27 +3,30 @@ package br.com.fiap.roomwordsample
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.roomwordsample.adapter.WordListAdapter
 import br.com.fiap.roomwordsample.model.Word
-
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    val adapter : WordListAdapter by inject()
+
+    val wordViewModel: WordViewModel by viewModel()
+
     companion object {
         const val newWordActivityRequestCode = 1
-    }
 
-    private lateinit var wordViewModel: WordViewModel
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +34,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
 
         wordViewModel.allWords.observe(this, Observer { words ->
             // Update the cached copy of the words in the adapter.
--- app/src/main/java/br/com/fiap/roomwordsample/WordViewModel.kt
+++ app/src/main/java/br/com/fiap/roomwordsample/WordViewModel.kt
@@ -4,22 +4,14 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-import br.com.fiap.roomwordsample.dao.WordRoomDatabase
 import br.com.fiap.roomwordsample.model.Word
 import br.com.fiap.roomwordsample.repository.WordRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class WordViewModel(application: Application) : AndroidViewModel(application) {
+class WordViewModel(application: Application, val repository: WordRepository) : AndroidViewModel(application) {
 
-    private val repository: WordRepository
-    val allWords: LiveData<List<Word>>
-
-    init {
-        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
-        repository = WordRepository(wordsDao)
-        allWords = repository.allWords
-    }
+    val allWords: LiveData<List<Word>> = repository.allWords
 
     fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
         repository.insert(word)
